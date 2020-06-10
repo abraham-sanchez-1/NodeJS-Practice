460e072a7f40c3789806db095aab5eae2336ce2e
Abe Sanchez <sanchez95san@gmail.com>
2020-06-10 13:44:51 -0500

    introduction to yargs npm package, will be used consistently in this project to create commands

--- app.js
+++ app.js
@@ -1,12 +1,49 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const myNotes = require('./notes')
 
-const command = process.argv[2]
+//Customized yargs vesion
 
-console.log(process.argv)
+yargs.version('1.1.0')
 
-if (command === 'add') {
-    console.log('Adding note')
-} else if (command === 'remove') {
-    console.log('Removing note')
-}
+//Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log('Adding new note!')
+    }
+})
+
+//Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing the note!')
+    }
+})
+
+
+//Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List of notes',
+    handler: function() {
+        console.log('Listing all notes!')
+    }
+})
+
+
+//Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Reading notes',
+    handler: function() {
+        console.log('Reading notes!')
+    }
+})
+
+//add, remove, list, read
+
+console.log(yargs.argv)
