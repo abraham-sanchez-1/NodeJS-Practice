d7aba140c9a573ac8c91d467cd30e457711b7604
Abe Sanchez <sanchez95san@gmail.com>
2020-06-16 17:43:48 -0500

    key added via gitignore, end of initial portion of weather app set up, current weather data parsed to console

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,7 @@
 const request = require('request')
+const secrets = require('../secrets')
 
-const url = 'http://api.weatherstack.com/current?access_key=' + key + '&query=43.0389,-87.9065'
+const url = 'http://api.weatherstack.com/current?access_key=' + secrets.weatherStackKey + '&query=43.0389,-87.9065'
 
 request({url: url}, (error, response) => {
     const data = JSON.parse(response.body)
