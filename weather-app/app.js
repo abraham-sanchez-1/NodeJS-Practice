53cb0803d2c95fe5f354a816bd702981b4b7fa17
Abe Sanchez <sanchez95san@gmail.com>
2020-06-18 17:48:42 -0500

    Error handling for weather api request completed and tested, moving to geocoding now

--- weather-app/app.js
+++ weather-app/app.js
@@ -5,21 +5,29 @@ const mkeCoordinates = '43.0389,-87.9065'
 
 const searchLocation = 'Los%20Angeles'
 
-const url = 'http://api.weatherstack.com/current?access_key=' + secrets.weatherStackKey + '&query='+ mkeCoordinates+'&units=f'
+const weatherURL = 'http://api.weatherstack.com/current?access_key=' + secrets.weatherStackKey + mkeCoordinates +'&query='+'&units=f'
 
 const geoCodingURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ searchLocation+'.json?access_token='+ secrets.mapBoxKey +'&limit=1'
 
-// request({url: url, json: true}, (error, response) => {
-//     // console.log(response.body.current)
 
-//     console.log(response.body.current.weather_descriptions[0] + '. It is currently: ' + response.body.current.temperature +' degrees.')
-//     console.log('It feels like: ' + response.body.current.feelslike +' degrees.')
+//GETWEATHER
+request({url: weatherURL, json: true}, (error, response) => {
+    if(error){
+        console.log('Unable to connect to weather service!')
+    }else if (response.body.error) {
+        console.log('Unable to find location')
+    } else {
+        console.log(response.body.current.weather_descriptions[0] + '. It is currently: ' + response.body.current.temperature +' degrees.')
+        console.log('It feels like: ' + response.body.current.feelslike +' degrees.')
+    }
+    })
+
+
+//GEOCODING
+// request({url: geoCodingURL, json: true}, (error, response) => {
+//     const latidude = response.body.features[0].center[1]
+//     const longitude = response.body.features[0].center[0]
+//     console.log('Latitude is: ' + latidude + '   Longitude is: ' + longitude)
 // })
-
-request({url: geoCodingURL, json: true}, (error, response) => {
-    const latidude = response.body.features[0].center[1]
-    const longitude = response.body.features[0].center[0]
-    console.log('Latitude is: ' + latidude + '   Longitude is: ' + longitude)
-})
 
 
