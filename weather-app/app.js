6d0739ffb558b971e6c48e074e412f1e01036f7c
Abe Sanchez <sanchez95san@gmail.com>
2020-06-19 17:49:48 -0500

    Comments have been removed, main js file has been cleaned of unused code

--- weather-app/app.js
+++ weather-app/app.js
@@ -8,38 +8,13 @@ const searchLocation = 'Los%20Angeles'
 const weatherURL = 'http://api.weatherstack.com/current?access_key=' + secrets.weatherStackKey + mkeCoordinates +'&query='+'&units=f'
 const geoCodingURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ searchLocation+'.json?access_token='+ secrets.mapBoxKey +'&limit=1'
 
-
-//GETWEATHER
-// request({url: weatherURL, json: true}, (error, response) => {
-//     if(error){
-//         console.log('Unable to connect to weather service!')
-//     }else if (response.body.error) {
-//         console.log('Unable to find location')
-//     } else {
-//         console.log(response.body.current.weather_descriptions[0] + '. It is currently: ' + response.body.current.temperature +' degrees.')
-//         console.log('It feels like: ' + response.body.current.feelslike +' degrees.')
-//     }
-//     })
+geocode('Milwaukee', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+})
 
 
-
-
-// geocode('Milwaukee', (error, data) => {
-//     console.log('Error', error)
-//     console.log('Data', data)
-// })
-
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
-
-forecast(-75.7088, 44.1545, (error, data) => {
+forecast(44.1545,-75.7088, (error, data) => {
     console.log('Error', error)
     console.log('Data', data)
   })
--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -21,34 +21,3 @@ const forecast = (lat, long, callback) => {
 
 module.exports = forecast
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
-
-// forecast(-75.7088, 44.1545, (error, data) => {
-//     console.log('Error', error)
-//     console.log('Data', data)
-//   })
