a4bcd7bbeedfddbe5a1bf4a07da52d04069ba469
Abe Sanchez <sanchez95san@gmail.com>
2020-06-19 18:11:40 -0500

    Further code clean up in app.js for weather app, app now uses user input to dictace location of weather and geocoding requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,29 +1,25 @@
-const request = require('request')
-const secrets = require('../secrets')
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
+const request = require("request");
+const secrets = require("../secrets");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
-const mkeCoordinates = '43.0389,-87.9065'
-const searchLocation = 'Los%20Angeles'
-const weatherURL = 'http://api.weatherstack.com/current?access_key=' + secrets.weatherStackKey + mkeCoordinates +'&query='+'&units=f'
-const geoCodingURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ searchLocation+'.json?access_token='+ secrets.mapBoxKey +'&limit=1'
+const userLocation = process.argv[2];
 
-geocode('Milwaukee', (error, data) => {
-    if(error){
-        return console.log(error)
+if (!userLocation) {
+  console.log("Please provide location in terminal");
+} else {
+  geocode(userLocation, (error, data) => {
+    if (error) {
+      return console.log(error);
     }
 
     forecast(data.latitude, data.longitude, (error, forecastData) => {
-        if(error){
-            return console.log(error)
-        }
+      if (error) {
+        return console.log(error);
+      }
 
-        console.log(data.location)
-        console.log(forecastData)
-
-      })
-})
-
-
-
-
+      console.log(data.location);
+      console.log(forecastData);
+    });
+  });
+}
